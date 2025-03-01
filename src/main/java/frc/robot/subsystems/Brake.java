9ea8c1a050ad872f3a04367584a33f0c9248fc39
MrCollen <toddcollen@yahoo.com>
2025-03-01 14:10:08 -0500

    break fix

--- src/main/java/frc/robot/subsystems/Brake.java
+++ src/main/java/frc/robot/subsystems/Brake.java
@@ -4,14 +4,49 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.BrakeConstants;
+
 
 public class Brake extends SubsystemBase {
-  /** Creates a new Brake. */
-  public Brake() {}
+  /** Creates a new Hand. */
+  public static final Servo BRAKE_SERVOS = new Servo(0);
+  //private static final Servo BRAKE_SERVO_RIGHT = RobotMap.BRAKE_SERVOS_RIGHT;
+  //private static final Servo BRAKE_SERVO_LEFT = RobotMap.BRAKE_SERVOS_LEFT;
+
+  public Brake() {
+    setBrakePosition(Constants.BrakeConstants.BRAKE_ON);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Brake Angle", getBrakeAngle());
+
+    if (DriverStation.getMatchTime() < .1  && DriverStation.getMatchTime() >= 0 )
+    {
+      Brake.setBrakePosition(BrakeConstants.BRAKE_ON);
+    }
   }
+
+  /**
+   * exception if distance is not within reasonable rage or not in correct state
+   * 
+   * @param position set the hands to postion
+   */
+  public static void setBrakePosition(double position) {
+    BRAKE_SERVOS.setAngle(position); 
+  }
+
+
+  // Get the current position of one of the hands/servos
+  public static double getBrakeAngle() {
+    return BRAKE_SERVOS.getAngle();
+
+  }
+
 }
